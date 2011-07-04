d1110929fa973be0ef1b2453e07148481f11eeb2
Jace Bennett <jace.bennett@gmail.com>
2011-07-04 02:18:25 -0400

    Minor code cleanup.

--- Thorn/Conventions/ArgsHelpProvider.cs
+++ Thorn/Conventions/ArgsHelpProvider.cs
@@ -16,9 +16,8 @@ namespace Thorn.Conventions
 			}
 			else
 			{
-				var modelhelp =
-					new Args.Help.HelpProvider().GenerateModelHelp(
-						ArgsHelper.GetArgsModelBindingDefinitionForType(parameterType));
+				var provider = new Args.Help.HelpProvider();
+				var modelhelp = provider.GenerateModelHelp(ArgsHelper.GetArgsModelBindingDefinitionForType(parameterType));
 
 				var helpFormatter = new Args.Help.Formatters.ConsoleHelpFormatter(80, 1, 5);
 
